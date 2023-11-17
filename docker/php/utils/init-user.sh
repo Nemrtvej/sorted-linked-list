eca63cf871b0e8998210927395620e70c70cc436
Marek Makovec <z0mbas@gmail.com>
2023-11-17 18:41:13 +0100

    feat(#5): Add github ci (#14)
    * ci: Reduce overall docker image size
    
    * ci: add ci steps
    
    ---------
    
    Co-authored-by: Marek Makovec <marovi@spadnul.net>

--- docker/php/utils/init-user.sh
+++ docker/php/utils/init-user.sh
@@ -6,7 +6,5 @@ PUID="${PUID:-1000}"
 PGID="${PGID:-1000}"
 
 echo "Creating user www-data with ${PUID}:${PGID}"
-groupadd -g ${PGID} www-data
-useradd -m -u ${PUID} -g ${PGID} www-data
-
-chsh -s /bin/bash www-data
+addgroup -g ${PGID} www-data
+adduser -h /app -D -s /bin/bash -u ${PUID} -G www-data www-data
